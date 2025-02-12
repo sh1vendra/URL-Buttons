3380e4b112ca489e1f4b5a71ee2d92c6d2659e6c
sh1vendra <shivendra@txstate.edu>
2025-02-12 10:54:53 -0600

    Remove openBrowser method from ParentURLButton

--- buttoninheritance/urlbuttons/ParentURLButton.java
+++ buttoninheritance/urlbuttons/ParentURLButton.java
@@ -30,18 +30,6 @@ public class ParentURLButton extends ButtonUI implements ActionListener{
     }
 
 
-    private void openBrowser(String url) {
-        if (Desktop.isDesktopSupported()) {
-            try {
-                Desktop.getDesktop().browse(new URI(url));
-            } catch (IOException | URISyntaxException ex) {
-                ex.printStackTrace(System.out);
-            }
-        } else {
-            System.out.println("Desktop is not supported on this platform.");
-        }
-    }
 
-}
 
 
