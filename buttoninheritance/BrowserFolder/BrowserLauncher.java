dd44b34ad189195cf5662bd44f379386b9c01641
sh1vendra <shivendra@txstate.edu>
2025-02-12 11:00:07 -0600

    Add openBrowser method in BrowserLauncher.java

--- buttoninheritance/BrowserFolder/BrowserLauncher.java
+++ buttoninheritance/BrowserFolder/BrowserLauncher.java
@@ -4,6 +4,19 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-  
+public class BrowserLauncher {
+        private void openBrowser(String url) {
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (IOException | URISyntaxException ex) {
+                ex.printStackTrace(System.out);
+            }
+        } else {
+            System.out.println("Desktop is not supported on this platform.");
+        }
+    }
+
+}
     
 
