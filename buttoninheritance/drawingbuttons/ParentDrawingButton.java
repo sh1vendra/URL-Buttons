257dc58bc29a494f02da3921ae17f12b112bcf1e
sh1vendra <shivendra@txstate.edu>
2025-02-12 18:21:01 -0600

    ParentDrawing inherits from ButtonUI

--- buttoninheritance/drawingbuttons/ParentDrawingButton.java
+++ buttoninheritance/drawingbuttons/ParentDrawingButton.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class ParentDrawingButton {
+public abstract class ParentDrawingButton extends ButtonUI{
 
 
     public ParentDrawingButton(String text) {
@@ -31,3 +31,4 @@ public class ParentDrawingButton {
         plotterFrame.setVisible(true);
     }
 }
+
